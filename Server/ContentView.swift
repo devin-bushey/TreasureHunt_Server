d42efdb086f698bc7bacddf4fc302e8529b3a351
Devin B <ics20-028@camosun.bc.ca>
2022-04-08 13:22:41 -0700

    Created working Server for 2 players

--- Server/ContentView.swift
+++ Server/ContentView.swift
@@ -11,11 +11,6 @@ struct ContentView: View {
     @State var advertising = false
     @StateObject var networkSupport = NetworkSupport(browse: false)
     
-    var numColumns = 5
-    var numRows = 5
-    var numTreasures = 5
-    //var treasureArray : [[Int]]
-    
     var body: some View {
         VStack {
             if !advertising {
@@ -25,21 +20,13 @@ struct ContentView: View {
                 }
             }
             else {
-                Text(networkSupport.incomingMessage)
-                    .padding()
-                
-                if networkSupport.connected {
-                    Button("Reply") {
-                        //networkSupport.send(message: "Thank you for: " + networkSupport.incomingMessage)
-                        //let reply = "Here is the board: "
-                        let flattened = initializeArray().flatMap { $0 }
-                        let stringArray = flattened.map { String($0) }
-                        //reply += stringArray
-                        networkSupport.send(message: "Here is the board: " + stringArray.joined(separator: ", "))
-                    }
-                    .padding()
+                VStack{
+                    Text("Test")
+                    Text("Is Player 1's turn? " + String(networkSupport.isPlayer1Turn))
+                    Text("Network Peer Name: " + networkSupport.peerName)
+                    Text("Incoming Message: " + networkSupport.incomingMessage)
+                    Text("Number of peers connected: " + String(networkSupport.peers.count))
                 }
-                
                 Button("Stop") {
                     networkSupport.nearbyServiceAdvertiser?.stopAdvertisingPeer()
                     advertising.toggle()
@@ -49,20 +36,8 @@ struct ContentView: View {
         }
         .padding()
     }
+
     
-    func initializeArray() -> [[Int]]{
-        
-        // initialize array to be all 0's
-        var treasureArray = Array(repeating: Array(repeating: 0, count: numColumns), count: numRows)
-        
-        // place n number of treasures
-        for _ in 0...numTreasures {
-            let randX = Int.random(in: 1..<numRows)
-            let randY = Int.random(in: 1..<numColumns)
-            treasureArray[randX][randY] = 1
-        }
-        
-        return treasureArray
-        
-    }
 }
+
+
